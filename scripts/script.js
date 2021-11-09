685dbd9386daa11816f0f7afefacf2917266ddc5
Aaron Huggins <huggins.aaron@outlook.com>
2021-11-09 11:17:21 -0800

    Fixed multi digit number bug. Subtract works

--- scripts/script.js
+++ scripts/script.js
@@ -17,13 +17,22 @@ function add(displayValue,storedValue) {
   //If statement stops the button from doubling the stored value
   if (currentOperator !== 'add') {
     currentOperator = 'add'; //<-- Set current operator to 'add', to be used by operate().
-    storedValue.push(displayValue.join('')); //<-- Add the current display value into the stored value array.
+    storedValue.push(displayValue.join('')); //<-- Push the current display value into the stored value array.
     displayPopulated = false; //<-- Have next number entry overwrite the display.
   }
 }
 
+//When - is pressed
+document.getElementById('-').addEventListener('click',function() {
+  sub(displayValue,storedValue);
+});
 function sub(displayValue,storedValue) {
-  return displayValue - storedValue;
+  //If statement stops the button from doubling the stored value
+  if (currentOperator !== 'sub') {
+    currentOperator = 'sub'; //<-- Set current operator to 'add', to be used by operate().
+    storedValue.push(displayValue.join('')); //<-- Push the current display value into the stored value array.
+    displayPopulated = false; //<-- Have next number entry overwrite the display.
+  }
 }
 
 function mul(displayValue,storedValue) {
@@ -40,6 +49,8 @@ document.getElementById('=').addEventListener('click',function(){
 })
 //Takes the displayed value and stored values, and performs an operation based on the current operator.
 function operate(operator,displayed,stored) {
+    //Combine displayed digits into a single number, and convert from string to integer.
+    displayed = displayed.join('');
     switch(operator) {
       case 'add': 
       //If statement prevents repeated = presses to cause a logic error.
@@ -47,8 +58,11 @@ function operate(operator,displayed,stored) {
         stored = stored.map(Number); //<-- Convert values in stored to integers.
         const add = (previousValue, currentValue) => previousValue + currentValue;
         stored = stored.reduce(add,0) //<-- Add all the values in stored.
-        displayed[0] += stored; //<-- Add the value in stored to the value currently displayed.
-        display.innerHTML = displayed[0]; //<-- Display the new value.
+        displayed = parseInt(displayed);
+        displayed += stored; //<-- Add the value in stored to the value currently displayed.
+        display.innerHTML = displayed; //<-- Display the new value.
+        displayValue = [];
+        displayValue.push(displayed);
         storedValue = []; //<-- Clear storedValue, as the new sum is in displayValue.
         displayPopulated = false; //<-- Next number entry will overwrite the display.
       }
@@ -60,8 +74,29 @@ function operate(operator,displayed,stored) {
         //Reset the current operator to avoid doubling stored values.
         currentOperator = null;
         break;
-        
-      case 'sub': return sub(displayValue,storedValue);break;
+
+      case 'sub': 
+        //If statement prevents repeated = presses to cause a logic error.
+        if (stored.length !== 0) {
+          stored = stored.map(Number); //<-- Convert values in stored to integers.
+          const add = (previousValue, currentValue) => previousValue + currentValue;
+          stored = stored.reduce(add,0) //<-- Add all the values in stored.
+          displayed = parseInt(displayed);
+          displayed = stored - displayed; //<-- Add the value in stored to the value currently displayed.
+          display.innerHTML = displayed; //<-- Display the new value.
+          displayValue = [];
+          displayValue.push(displayed);
+          storedValue = []; //<-- Clear storedValue, as the new sum is in displayValue.
+          displayPopulated = false; //<-- Next number entry will overwrite the display.
+        }
+        else {
+          //If stored is already cleared, just display what's already being displayed.
+          display.innerHTML = displayed[0]; 
+          displayPopulated = false;
+        }
+          //Reset the current operator to avoid doubling stored values.
+          currentOperator = null;
+          break;
       case 'mul': return mul(displayValue,storedValue);break;
       case 'div': return div(displayValue,storedValue);break;
       default:break;
